664196b38f3bebfbb6e3284a4a738537a6c77c98
Pavel Sasim <pavels324@gmail.com>
2024-10-01 16:17:53 +0300

    made task 6

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,16 @@ function App() {
         { id: 1, title: 'HTML&CSS', isDone: true },
         { id: 2, title: 'JS', isDone: true },
         { id: 3, title: 'ReactJS', isDone: false },
+        { id: 4, title: 'Redux', isDone: false },
+        { id: 5, title: 'Typescript', isDone: false },
+        { id: 6, title: 'RTK query', isDone: false },
     ]
 
     const tasks2: TaskType[] = [
         { id: 1, title: 'Hello world', isDone: true },
         { id: 2, title: 'I am Happy', isDone: false },
         { id: 3, title: 'Yo', isDone: false },
+        // { id: 4, title: 'Redux', isDone: false },
     ]
 
     return (
